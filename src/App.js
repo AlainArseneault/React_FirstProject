16be9fa95e55647af23871e5a582104ab750c486
Alain A <alarseneault@hotmail.com>
2021-11-24 13:43:32 -0500

    Created project

--- src/App.js
+++ src/App.js
@@ -5,19 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome To my Website</h1>
       </header>
+      <main>
+        <p>This will show my learning of the react language</p>
+      </main>
     </div>
   );
 }
